7da7da478f17550594d794f9d1508efcb27158ef
S-Spence <sarah.spence1991@gmail.com>
2021-08-16 15:31:52 -0400

    Code refactor

--- src/components/object_detector/index.jsx
+++ src/components/object_detector/index.jsx
@@ -1,72 +1,7 @@
 import React, { useRef, useState } from "react";
-import styled from "styled-components";
-
 import "@tensorflow/tfjs-backend-cpu";
-//import "@tensorflow/tfjs-backend-webgl";
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
-
-const ObjectDetectorContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const DetectorContainer = styled.div`
-  min-width: 300px;
-  height: 600px;
-  border: 3px solid #fff;
-  border-radius: 5px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-`;
-
-const TargetImg = styled.img`
-  height: 100%;
-`;
-
-const HiddenFileInput = styled.input`
-  display: none;
-`;
-
-const SelectButton = styled.button`
-  padding: 7px 10px;
-  border: 2px solid transparent;
-  background-color: #fff;
-  color: #0a0f22;
-  font-size: 16px;
-  font-weight: 500;
-  outline: none;
-  margin-top: 2em;
-  cursor: pointer;
-  transition: all 260ms ease-in-out;
-  &:hover {
-    background-color: transparent;
-    border: 2px solid #fff;
-    color: #fff;
-  }
-`;
-
-const TargetBox = styled.div`
-  position: absolute;
-  left: ${({ x }) => x + "px"};
-  top: ${({ y }) => y + "px"};
-  width: ${({ width }) => width + "px"};
-  height: ${({ height }) => height + "px"};
-  border: 4px solid #1ac71a;
-  background-color: transparent;
-  z-index: 20;
-  &::before {
-    content: "${({ classType, score }) => `${classType} ${score.toFixed(1)}%`}";
-    color: #1ac71a;
-    font-weight: 500;
-    font-size: 17px;
-    position: absolute;
-    top: -1.5em;
-    left: -5px;
-  }
-`;
+import "./index.css";
 
 export function ObjectDetector(props) {
   const fileInputRef = useRef();
@@ -141,12 +76,14 @@ export function ObjectDetector(props) {
   };
 
   return (
-    <ObjectDetectorContainer>
-      <DetectorContainer>
-        {imgData && <TargetImg src={imgData} ref={imageRef} />}
+    <div className= {`object-detector-container ${ObjectDetector}`}>
+      <div className = {`detector-container ${ObjectDetector}`}>
+        {imgData && 
+          <div className ={`target-image ${ObjectDetector}`} src={imgData} ref={imageRef} />
+        }
         {!isEmptyPredictions &&
           predictions.map((prediction, idx) => (
-            <TargetBox
+            <div className = {`target-box ${ObjectDetector}`}>
               key={idx}
               x={prediction.bbox[0]}
               y={prediction.bbox[1]}
@@ -154,17 +91,18 @@ export function ObjectDetector(props) {
               height={prediction.bbox[3]}
               classType={prediction.class}
               score={prediction.score * 100}
-            />
-          ))}
-      </DetectorContainer>
-      <HiddenFileInput
+            </ div>
+          ))
+        } 
+      </ div>
+      <div className = {`hidden-file-input ${ObjectDetector}`}>
         type="file"
         ref={fileInputRef}
         onChange={onSelectImage}
-      />
-      <SelectButton onClick={openFilePicker}>
+      </ div>
+      <div className= {`select-button ${ObjectDetector}`} onClick={openFilePicker}>
         {isLoading ? "Recognizing..." : "Select Image"}
-      </SelectButton>
-    </ObjectDetectorContainer>
+      </ div>
+    </ div>
   );
 }
